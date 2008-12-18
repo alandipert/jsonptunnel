f2874acaf8b16f64cb79b4f39d42f06e2f4b2cd5
Alan Dipert <alan@ubergibson.com>
2008-12-18 09:59:13 -0800

    error prints its own content-type (text/plain)

--- error.c
+++ error.c
@@ -6,6 +6,7 @@
  * Send a '400' 'Bad Request' HTTP error code and exit.
  */
 void exitStatus(int status, char *msg) {
+  cgiHeaderContentType("text/plain");
   cgiHeaderStatus(status, msg);
   fprintf(cgiOut, "There was an error processing your request: %s\n", msg);
 }
